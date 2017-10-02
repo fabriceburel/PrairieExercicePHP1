84bca15682a1737ac1ff831b892be1fd11612de9
Fabrice BUREL <burelfabrice35@gmail.com>
2017-10-02 09:34:09 +0200

    correction exercice1

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-$chiffre1=15;
-$chiffre2=20;
-$somme=$chiffre1+$chiffre2;
-$produit=$chiffre1*$chiffre2;
-echo "la somme des 2 chiffres est: $somme et le produit est $produit";
-
- ?>
+  $chiffre1=15;
+  $chiffre2=20;
+  $somme=$chiffre1+$chiffre2;
+  $produit=$chiffre1*$chiffre2;
+  echo 'la somme des 2 chiffres est: '. $somme . ' et le produit est: ' . $produit;
+?>
